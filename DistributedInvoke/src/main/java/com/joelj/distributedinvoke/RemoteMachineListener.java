73564330f5132a3b7b8b748f35e2341fa4be548d
joeljohnson <joeljohnson@attask.com>
2013-03-02 12:34:55 -0700

    Added a machine listener. This is essentially the the slave machine. It will listen for a machine to connect to it, then wait for it to send it commands.

---
+++ DistributedInvoke/src/main/java/com/joelj/distributedinvoke/RemoteMachineListener.java
@@ -0,0 +1,146 @@
+package com.joelj.distributedinvoke;
+
+import com.joelj.distributedinvoke.logging.Logger;
+import com.joelj.ezasync.EzAsync;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Callable;
+
+/**
+ * User: Joel Johnson
+ * Date: 3/2/13
+ * Time: 12:36 AM
+ */
+public class RemoteMachineListener implements Closeable {
+	private static final Logger LOGGER = Logger.forClass(RemoteMachineListener.class);
+
+	private final InetAddress bindAddress;
+	private final int listeningPort;
+	private final transient Thread thread;
+
+	private final transient EzAsync ezAsync;
+
+	/**
+	 * @param bindAddress The address to bind to.
+	 * @param listeningPort Must be between 1 and 65535. On some operating systems, if the value is between 1 and 1024 the underlying JVM may need special privileges to open the socket.
+	 */
+	private RemoteMachineListener(@NotNull InetAddress bindAddress, int listeningPort) {
+		this.bindAddress = bindAddress;
+		this.listeningPort = listeningPort;
+		thread = new Thread(new ListeningThread(), this.getClass().getSimpleName());
+		thread.start();
+
+		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+			@Override
+			public void run() {
+				close();
+			}
+		}));
+
+		ezAsync = EzAsync.create();
+	}
+
+	@Override
+	public void close() {
+		thread.interrupt();
+	}
+
+	private class ListeningThread implements Runnable {
+		@Override
+		public void run() {
+			ServerSocket serverSocket;
+			try {
+				serverSocket = new ServerSocket(listeningPort, 0, bindAddress);
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+
+			Socket clientSocket = null;
+			while(!Thread.interrupted()) {
+				if(clientSocket == null) {
+					//Either this is the first iteration, or we lost connection with the client and we should wait for it to reconnect.
+					try {
+						clientSocket = serverSocket.accept();
+					} catch (IOException e) {
+						throw new RuntimeException(e);
+					}
+				}
+
+				assert clientSocket != null;
+
+				ObjectInputStream inputStream;
+				try {
+					inputStream = new ObjectInputStream(clientSocket.getInputStream());
+				} catch (IOException e) {
+					LOGGER.warn("IOException. Retrying connection.", e);
+					LOGGER.info("Attempting to close current socket.");
+					try {
+						clientSocket.close();
+					} catch (IOException closeException) {
+						LOGGER.info("Couldn't close current socket.", closeException);
+					}
+					clientSocket = null;
+					continue;
+				}
+
+				Object object;
+				try {
+					object = inputStream.readObject();
+				} catch (IOException e) {
+					LOGGER.error(e);
+					continue;
+				} catch (ClassNotFoundException e) {
+					LOGGER.error("Class path is out of sync", e);
+					continue;
+				}
+
+				if(object != null && object instanceof Transport) {
+					Transport transport = (Transport)object;
+					String requestId = transport.getId();
+					Object requestObject = transport.getObject();
+
+					if(requestObject instanceof Callable) {
+						//noinspection unchecked
+						ezAsync.execute((Callable<Object>) requestObject, new RequestCallback(requestId, clientSocket));
+					} else {
+						LOGGER.error("Unexpected object type. Expected " + Callable.class.getCanonicalName() + " but was " + (requestObject == null ? "null" : requestObject.getClass().getCanonicalName()));
+					}
+
+				} else {
+					LOGGER.error("Unexpected object type. Expected " + Transport.class.getCanonicalName() + " but was " + (object == null ? "null" : object.getClass().getCanonicalName()));
+				}
+			}
+
+			LOGGER.info("Listener shut down.");
+		}
+	}
+
+	private static class RequestCallback implements EzAsync.Callback {
+		@NotNull
+		private final String id;
+
+		@NotNull
+		private final Socket clientSocket;
+
+		public RequestCallback(@NotNull String id, @NotNull Socket clientSocket) {
+			this.id = id;
+			this.clientSocket = clientSocket;
+		}
+
+		@Override
+		public void done(@Nullable Object result) {
+			Transport<Object> response = Transport.wrapWithId(result, id);
+			try {
+				ObjectOutputStream outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
+				outputStream.writeObject(response);
+			} catch (IOException e) {
+				LOGGER.error("Couldn't write response.", e);
+			}
+		}
+	}
+}
