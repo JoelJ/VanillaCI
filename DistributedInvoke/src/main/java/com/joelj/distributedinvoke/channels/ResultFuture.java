f9703e1229e9c3d4f218659833852dfc98ae34d4
joeljohnson <joeljohnson@attask.com>
2013-03-04 22:33:15 -0700

    Adding the ability to register callbacks with ResultFuture instances. If a callback is registered and the value is already set, the callback is immediately called. Generifying ResultFuture

--- DistributedInvoke/src/main/java/com/joelj/distributedinvoke/channels/ResultFuture.java
+++ DistributedInvoke/src/main/java/com/joelj/distributedinvoke/channels/ResultFuture.java
@@ -1,7 +1,12 @@
 package com.joelj.distributedinvoke.channels;
 
 import com.joelj.distributedinvoke.Lock;
+import com.joelj.distributedinvoke.exceptions.UnexpectedResultException;
+import com.joelj.ezasync.EzAsync;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Represents an return value that hasn't been populated yet and will be by another thread.
@@ -10,10 +15,11 @@ import org.jetbrains.annotations.Nullable;
  * Date: 3/2/13
  * Time: 11:06 AM
  */
-public final class ResultFuture {
-	private volatile Object result;
+public final class ResultFuture<T> {
+	private volatile T result;
 	private volatile boolean set;
 	private final Lock waitLock = new Lock();
+	private final List<EzAsync.Callback<T>> callbacks = new LinkedList<EzAsync.Callback<T>>();
 
 	/* package */ ResultFuture() {
 
@@ -26,7 +32,7 @@ public final class ResultFuture {
 	 * @throws InterruptedException
 	 */
 	@Nullable
-	public Object waitForResult() throws InterruptedException {
+	public T waitForResult() throws InterruptedException {
 		if(!set) {
 			synchronized (waitLock) {
 				if(!set) {
@@ -37,14 +43,20 @@ public final class ResultFuture {
 		if(!set) {
 			throw new IllegalStateException("Notified but never set: " + this);
 		}
-		return result;
+
+		try {
+			//noinspection unchecked
+			return (T)result;
+		} catch (ClassCastException e) {
+			throw new UnexpectedResultException("Return type was not what was expected. Received: " + result);
+		}
 	}
 
 	/**
 	 * Sets the result of the future and notifies all callers of {@link #waitForResult()}. Can only be called once.
 	 * @param result The value to be set. Can be null.
 	 */
-	/*package*/ void setResult(@Nullable Object result) {
+	/*package*/ void setResult(@Nullable T result) {
 		if(set) {
 			throw new IllegalStateException("Value already set: " + this + " to " + String.valueOf(result));
 		}
@@ -52,8 +64,24 @@ public final class ResultFuture {
 		this.result = result;
 		set = true;
 
+		synchronized (callbacks) {
+			for (EzAsync.Callback<T> callback : callbacks) {
+				callback.done(this.result);
+			}
+		}
+
 		synchronized (waitLock) {
 			waitLock.notifyAll();
+		}
+	}
+
+	public void registerCallback(EzAsync.Callback<T> callback) {
+		if(set) {
+			callback.done(this.result);
+		} else {
+			synchronized (callbacks) {
+				callbacks.add(callback);
+			}
 		}
 	}
 }
